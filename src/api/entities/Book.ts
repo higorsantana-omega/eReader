86ea2caef7f666fdee8bee4e0277556a6eab4532
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-04 18:06:47 -0300

    feat: entity book

---
+++ src/api/entities/Book.ts
@@ -0,0 +1,18 @@
+class Book {
+  id: string;
+  book_name: string;
+  author: string;
+  categories: string;
+  book_rating: number;
+  publisher: string;
+  year: number;
+  pages: number;
+  volume: number;
+  edition: number;
+  language: string;
+  file: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export { Book };
