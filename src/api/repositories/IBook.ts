5c44a67db4a94a4a70ded3f1237e5596741aec02
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-05 16:57:03 -0300

    feat: add interface of book

---
+++ src/api/repositories/IBook.ts
@@ -0,0 +1,31 @@
+class IBook {
+  id?: string;
+
+  book_name: string;
+
+  author: string;
+
+  categories: string;
+
+  book_rating: number;
+
+  publisher: string;
+
+  year: number;
+
+  pages: number;
+
+  volume: number;
+
+  edition: number;
+
+  language: string;
+
+  file: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
+
+export { IBook };
