629dd38a1108f0c6ef57f4ce9306a82a54d8276f
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-05 01:44:49 -0300

    feat: add entity book

--- src/api/entities/Book.ts
+++ src/api/entities/Book.ts
@@ -1,17 +1,47 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
 class Book {
-  id: string;
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
   book_name: string;
+
+  @Column()
   author: string;
+
+  @Column()
   categories: string;
+
+  @Column()
   book_rating: number;
+
+  @Column()
   publisher: string;
+
+  @Column()
   year: number;
+
+  @Column()
   pages: number;
+
+  @Column()
   volume: number;
+
+  @Column()
   edition: number;
+
+  @Column()
   language: string;
+
+  @Column()
   file: string;
+
+  @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
