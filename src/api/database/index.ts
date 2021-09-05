506353294a27b001a0a7caacb57e2f0a8d4eab79
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-05 13:45:06 -0300

    del: typeorm

--- src/api/database/index.ts
+++
@@ -1,7 +0,0 @@
-import { createConnection, getConnectionOptions } from "typeorm";
-
-console.log('Loading Database')
-export default async () => {
-  const defaultOptions = await getConnectionOptions();
-  return createConnection(Object.assign(defaultOptions))
-}
