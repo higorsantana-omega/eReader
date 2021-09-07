6ac73d50a72010eaa3b7bdd2c0a12df7d2c8142d
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-07 17:21:11 -0300

    feat: add books in database

--- src/api/books.routes.ts
+++ src/api/books.routes.ts
@@ -1,12 +1,55 @@
 import { Request, Response, Router } from "express";
+import { listenerCount } from "stream";
+import { app } from "./app";
 import { Book } from "./entities/Book";
+import { IBook } from "./repositories/IBook";
 
-const router = Router()
+const router = Router();
 
 router.get("/", async (req: Request, res: Response): Promise<Response> => {
-  const booksList = await Book.findAll()
-  
-  return res.json(booksList)
-})
+  const booksList = await Book.findAll();
 
-export { router }
+  return res.json(booksList);
+});
+
+router.post("/book", async (req: Request, res: Response): Promise<Response> => {
+  const {
+    id,
+    book_name,
+    author,
+    categories,
+    book_rating,
+    publisher,
+    year,
+    pages,
+    volume,
+    edition,
+    language,
+    file,
+  }: IBook = req.body;
+
+  let findEqualsID = await Book.findByPk(id)
+
+  if (findEqualsID) {
+    return res.status(400).send("Este livro já existe")
+  }
+
+  let createBook = await Book.create({
+    id,
+    book_name,
+    author,
+    categories,
+    book_rating,
+    publisher,
+    year,
+    pages,
+    volume,
+    edition,
+    language,
+    file,
+  })
+
+  return res.json(createBook)
+});
+
+export { router };
