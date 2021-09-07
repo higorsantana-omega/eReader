d2784f9b5832deab6bb27855e87fbb1ee3a4b595
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-07 17:48:49 -0300

    feat: add update book

--- src/api/books.routes.ts
+++ src/api/books.routes.ts
@@ -28,10 +28,10 @@ router.post("/book", async (req: Request, res: Response): Promise<Response> => {
     file,
   }: IBook = req.body;
 
-  let findEqualsID = await Book.findByPk(id)
+  let findEqualsID = await Book.findByPk(id);
 
   if (findEqualsID) {
-    return res.status(400).send("Este livro já existe")
+    return res.status(400).send("Este livro já existe");
   }
 
   let createBook = await Book.create({
@@ -47,9 +47,54 @@ router.post("/book", async (req: Request, res: Response): Promise<Response> => {
     edition,
     language,
     file,
-  })
+  });
 
-  return res.json(createBook)
+  return res.json(createBook);
+});
+
+router.put("/book/:idbook", async (req: Request, res: Response) => {
+  const { idbook } = req.params;
+  const {
+    id,
+    book_name,
+    author,
+    categories,
+    book_rating,
+    publisher,
+    year,
+    pages,
+    volume,
+    edition,
+    language,
+    file,
+  } = req.body;
+
+  if (!idbook) {
+    return res.status(400).json("Informe o id");
+  }
+
+  const findBookById = await Book.findByPk(idbook);
+
+  if (!findBookById) {
+    return res.status(400).json("Este livro não existe");
+  }
+
+  const updateBook = await findBookById.update({
+    id,
+    book_name,
+    author,
+    categories,
+    book_rating,
+    publisher,
+    year,
+    pages,
+    volume,
+    edition,
+    language,
+    file,
+  });
+
+  return res.status(200).json(updateBook)
 });
 
 export { router };
