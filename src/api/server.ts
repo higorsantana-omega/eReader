5c72f543dbbb8481bd84472259b106fdced45db9
higorsantana-omega <omegadosgames02@gmail.com>
2021-09-19 23:37:41 -0300

    feat: add symbol to indicate server is running

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import logSymbols from "log-symbols";
 import { app } from "./app";
 
-app.listen(2001, () => console.log("Servidor rodando"));
+app.listen(2001, () => console.log(logSymbols.success, "Server is running!"));
