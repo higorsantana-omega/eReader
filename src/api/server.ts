bf6f85af79cb1ca93138d980a5f7b8b3fd6f8520
higorsantana-omega <omegadosgames02@gmail.com>
2021-09-21 00:32:10 -0300

    feat: implement log

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
 import { app } from "./app";
+import { logger } from "./helpers/log";
 
-app.listen(2001, () => console.log("Server is running!"));
+app.listen(2001, () => logger.info("Server is running!"));
