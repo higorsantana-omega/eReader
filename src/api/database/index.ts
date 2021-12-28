923337afa787c1b398d079c0e9a7198bd79d354f
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2021-12-28 01:26:35 -0300

    fix: del imports unnecessarys and add logs

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -1,15 +1,13 @@
-import { Book } from "../entities/Book";
-import { Sequelize } from "sequelize";
 import { connection } from "./connection";
+import { logger } from "../helpers/log"; 
 
 export default (async () => {
   const db = connection
-  const db_table = Book
   try {
     await db.sync()
-    console.log('oi')
+    logger.info('DB is now connected')
   } catch (err) {
-    console.log(err)
+    logger.fatal('DB: ', err)
   }
 })
 
